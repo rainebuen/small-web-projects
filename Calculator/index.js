21271a50dbfbe5e213899461fafb7a2e9576632d
Lhoraine Buen <135022428+rainebuen@users.noreply.github.com>
2025-08-09 18:22:53 +0800

    Add files via upload

---
+++ Calculator/index.js
@@ -0,0 +1,26 @@
+// Calculator
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+ display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+
+    catch(error) {
+        display.value = "Error";
+    }
+    
+}
+
+function deleteKey() {
+     display.value = display.value.slice(0, -1);
+}
